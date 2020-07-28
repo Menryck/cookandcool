5e09604138f9b2b801c55ec524773f7af4d5a9b2
gvetvn <gaetanchretien@hotmail.com>
2020-07-28 12:32:30 +0200

    modf-ingredient

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -6,6 +6,8 @@ use App\Entity\Ingredient;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class IngredientType extends AbstractType
 {
@@ -13,7 +15,19 @@ class IngredientType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('image')
+            ->add('image', FileType::class,[
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10240k',
+                        'mimeTypes' => [
+                            'image/*',
+                        ],
+                        'mimeTypesMessage' => 'désolé ce format est interdit',
+                    ])
+                ],
+            ])
             ->add('calories')
             ->add('unite')
             ->add('typeIngredient')
