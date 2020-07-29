836f78651698cc751f0415a4a2136f21f20e54b3
Jeremy Prin-Derre <menryck@gmail.com>
2020-07-29 10:20:47 +0200

    ajout recette localstorage

--- public/assets/js/detail.js
+++ public/assets/js/detail.js
@@ -1,21 +1,41 @@
-console.log("JS2 détail recette en cours de chargement");
+console.log("JS détail recette en cours de chargement");
+
+// Ajout de la recette
+
+var titreRecette = document.getElementById("titre").innerHTML;
+var boutonAjout = document.getElementById("ajouter");
+
+boutonAjout.addEventListener("click", event => {
+    window.localStorage.setItem("plat", titreRecette); 
+    window.location.href = "../../menu/jours";
+});
+
+
+
+// Gestion du nombre de personnes
 
 var quantite = document.getElementById("quantite");
+var masque = document.getElementsByClassName("masque");
 var plus = document.getElementById("plus");
 var moins = document.getElementById("moins");
+var personnes = document.querySelectorAll("span.personnes");
 
 plus.addEventListener("click", event => {
-    quantite.innerHTML = parseInt(quantite.innerHTML)+1; });
+    quantite.innerHTML = parseInt(quantite.innerHTML)+1; 
+    for (let personne of personnes) {
+        personne.innerHTML = parseInt(masque.innerHTML)*parseInt(quantite.innerHTML);
+    }});
 moins.addEventListener("click", event => {
     if(quantite.innerHTML != 1) {
     quantite.innerHTML = parseInt(quantite.innerHTML)-1; 
+    for (let personne of personnes) {
+        personne.innerHTML = parseInt(masque.innerHTML)*parseInt(quantite.innerHTML);
     }
+}
     else {
         quantite.innerHTML = 1;
     }});
 
-let personnes = document.querySelectorAll("span.personnes");
-for (let personne of personnes) {
-    console.log(personne.innerHTML);
-}
+
+
 
