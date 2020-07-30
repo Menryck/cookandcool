533b9c64d9bf35f74848c26230acd90e036b12c2
gvetvn <gaetanchretien@hotmail.com>
2020-07-30 16:12:14 +0200

    modf-css-icon

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -10,10 +15,12 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(ContactRepository $contactRepository): Response
     {
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
     }
+
+
 }
