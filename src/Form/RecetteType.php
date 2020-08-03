5e2b408e377359e475837d24fddbcb7288c1924f
NeoQenXzin <keyntao99@yahoo.fr>
2020-08-03 14:37:33 +0200

    maj

--- src/Form/RecetteType.php
+++ src/Form/RecetteType.php
@@ -3,11 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Recette;
-// use App\Entity\TableRecetteIngredients;
 use Symfony\Component\Form\AbstractType;
 use App\Form\TableRecetteIngredientsType;
 use Symfony\Component\Form\FormBuilderInterface;
-// use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -19,39 +17,33 @@ class RecetteType extends AbstractType
     {
         $builder
             ->add('titre')
+            ->add('type')
+            ->add('categorie')
             ->add('difficulte')
             ->add('duree')
             ->add('cuisson')
+            ->add('instructions')
             ->add('photo', FileType::class,[
-                // unmapped fields can't define their validation using annotations
-                // in the associated entity, so you can use the PHP constraint classes
+            // unmapped fields can't define their validation using annotations
+            // in the associated entity, so you can use the PHP constraint classes
                 'constraints' => [
                     new File([
                         'maxSize' => '10240k',
                         'mimeTypes' => [
-                            'image/*',
+                        'image/*',
                         ],
                         'mimeTypesMessage' => 'désolé ce format est interdit',
                     ])
                 ],
             ])
-            // ->add('ingredientsRecette')
-            // ->add('quantite', EntityType::class, [
-            //     'class' => TableRecetteIngredients::class
-            // ])
-            ->add('instructions')
-            ->add('type')
-            ->add('categorie')
-            // ->add('nbrePart')
-        
-// form_collection
+                // form_collection
             ->add ('ingredient', CollectionType::class, [
-            'entry_type' => TableRecetteIngredientsType::class,
-            'label' => " ",
-            'entry_options' => ['label' => 'ingredient'],
-            'allow_add' => true,
-            'by_reference' => false,
-            'allow_delete' => true,
+                'entry_type' => TableRecetteIngredientsType::class,
+                'label' => " ",
+                'entry_options' => ['label' => 'ingredient'],
+                'allow_add' => true,
+                'by_reference' => false,
+                'allow_delete' => true,
             ]);
 // form_collection fin
     }
